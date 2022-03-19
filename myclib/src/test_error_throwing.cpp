58f2b42c7a39ea3ffd6daf79fa7dbee57e50cc0c
Pierre Krafft <kpierre@outlook.com>
2022-03-19 09:39:44 +0100

    Add new type of exception

--- myclib/src/test_error_throwing.cpp
+++ myclib/src/test_error_throwing.cpp
@@ -20,7 +20,22 @@ extern "C"
       throw 20;
     } catch(int my_error)
     {
-      std::cout << "equivalent_to_id test-throw caught " << my_error << std::endl;
+      std::cout << "equivalent_to_id test-throw 1 caught " << my_error << std::endl;
+    }
+    try {
+      std::cout << "equivalent_to_id test-throw 2" << std::endl;
+      for (int i = 0; i < 10;i++) {
+        std::cout << "." << std::endl;
+        volatile int x;
+        for (x = 0; x < 50000000; x++)
+        {
+            /*do nothing*/      
+        }
+      }
+      throw std::runtime_error("Test error.");
+    } catch(const std::runtime_error& my_error)
+    {
+      std::cout << "equivalent_to_id test-throw 2 caught " << my_error.what() << std::endl;
     }
     for (int j = 0; j < 10;j++) {
         std::cout << "." << std::endl;
